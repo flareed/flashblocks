705f9f2bf0ceafc095f989e221ba10e3fd87b91c
flareed <nullrenity@gmail.com>
2024-12-02 08:16:14 +0700

    Initial commit

---
+++ public/register/register.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const root_dir = process.cwd();
+const readfile = require(path.join(root_dir, './resources/js/readfile'));
+
+/* GET home page. */
+router.get('/', async function (req, res, next)
+{
+    try
+    {
+        const data = await readfile.getHTMLContent(path.join(__dirname, "register.html"))
+        res.render('layout', { title: 'Register', content: data });
+    }
+    catch (err)
+    {
+        res.status(500).send('Error rendering page');
+    }
+});
+
+module.exports = router;
